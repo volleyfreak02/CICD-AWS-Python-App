f284b6caea9238e951a74e2d105feb0bda1651a6
Kaivalya Shende <shendekaivalya2@gmail.com>
2025-06-09 22:30:37 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull hinata10/cicd-aws-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 hinata10/cicd-aws-python-app
