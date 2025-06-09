fd6ee6be0c940344e4930d8ff6a49d9fd820903b
Kaivalya Shende <shendekaivalya2@gmail.com>
2025-06-09 23:59:06 +0530

    updated

--- start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull hinata10/cicd-aws-python-app
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 hinata10/cicd-aws-python-app
